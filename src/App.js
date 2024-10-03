e8b890109d63d68e461474d26815913200290c7d
Sonali <129552432+Sonali9867@users.noreply.github.com>
2024-10-03 15:35:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import CourseSelectionPage from './pages/CourseSelectionPage';
+import ProjectSelectionPage from './pages/ProjectSelectionPage';
+import PreferenceViewingPage from './pages/PreferenceViewingPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/course-selection" element={<CourseSelectionPage />} />
+        <Route path="/project-selection" element={<ProjectSelectionPage />} />
+        <Route path="/preference-viewing" element={<PreferenceViewingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
